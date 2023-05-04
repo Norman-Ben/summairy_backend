fdd74c377e5abfba93e0d30c256c9c4ca540993c
Ben Norman <90580315+Norman-Ben@users.noreply.github.com>
2023-05-04 23:22:06 +0100

    express-async-handler to handle controller errors

--- controllers/summaryController.js
+++ controllers/summaryController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require('express-async-handler');
+
 // Desc: Get summaries
 // Route: GET /api/summaries
 // Access: Private
-const getSummaries = (req, res) => {
+const getSummaries = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get summaries' });
-};
+});
 
 // Desc: Create summary
 // Route: POST /api/summaries
 // Access: Private
-const createSummary = (req, res) => {
+const createSummary = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error('Please enter a summary');
   }
 
   res.status(200).json({ message: 'Create summary' });
-};
+});
 
 // Desc: Update summary
 // Route: PUT /api/summaries/:id
 // Access: Private
-const updateSummary = (req, res) => {
+const updateSummary = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update summary ${req.params.id}` });
-};
+});
 
 // Desc: Delete summary
 // Route: DELETE /api/summaries/:id
 // Access: Private
-const deleteSummary = (req, res) => {
+const deleteSummary = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete summary ${req.params.id}` });
-};
+});
 
 module.exports = {
   getSummaries,
