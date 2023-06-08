04da1c57ec8f9e8f8b5702c21e9d68e1fc4b1576
Norman-Ben <90580315+Norman-Ben@users.noreply.github.com>
2023-06-08 12:00:28 +0100

    configure cors for added security

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: 'http://127.0.0.1:5173/',
-  optionsSuccessStatus: 200,
+  origin: process.env.CORS_ORIGIN,
 };
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
