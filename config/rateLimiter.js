e51c8b718e094e0a9258f572475df276460191f1
Ben Norman <90580315+Norman-Ben@users.noreply.github.com>
2023-08-04 10:29:02 +0100

    fix ratelimit export bug

--- config/rateLimiter.js
+++ config/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 60 * 1000, // 1 hour window
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 50, // limit each IP to 100 requests per windowMs
 });
 
-module.exports = { limiter };
+module.exports = limiter;
